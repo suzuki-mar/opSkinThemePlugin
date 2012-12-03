29e44c5f198ce4cb6784a72c4cd813de23df8661
suzuki_mar <supasu145@gmail.com>
2012-12-03 11:37:03 +0900

    プラグインの管理画面に設定ページを作成するための雛形を作成した
    .gitkeepは必要なくなったので削除した

---
+++ apps/pc_backend/modules/opManagerSkinPlugin/actions/actions.class.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * opManagerSkinPlugin actions.
+ *
+ * @package    OpenPNE
+ * @subpackage opManagerSkinPlugin
+ * @author     Your name here
+ */
+class opManagerSkinPluginActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfWebRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    
+  }
+}
